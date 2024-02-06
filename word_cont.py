7a09e1162aaee09df5a13236eca0eca130970f19
gbuser <gregory.buser@gmail.com>
2024-02-05 23:08:05 -0600

    Add files via upload

---
+++ word_cont.py
@@ -0,0 +1,41 @@
+
+import requests
+from bs4 import BeautifulSoup
+import re
+from collections import Counter
+
+def get_data(url):
+    r = requests.get(url)
+    return r.text
+
+def get_text(urls):
+    text = ''
+    for url in urls:
+        data = get_data(url)
+        soup = BeautifulSoup(data, 'html.parser')
+        tags = ["p", "h1", "h2"]
+        for tag in tags:
+            for prose in soup.find_all(tag):
+                text += prose.get_text() + '\n'
+    return text.lower()
+
+def get_words(text):
+    pattern = r'\b[a-z\'-]+\b'
+    words = re.findall(pattern, text)
+    return words
+
+urls = ["https://www.py4e.com/html3/01-intro",
+        "https://www.py4e.com/html3/02-variables",
+        "https://www.py4e.com/html3/03-conditional"]
+text = get_text(urls)
+
+word_count = Counter(get_words(text))
+word_list = [(key, value) for key, value in word_count.items()]
+word_list.sort(key = lambda x: x[1], reverse = True)
+
+for words in word_list[:10]:
+    print (words)
+
+
+
+
